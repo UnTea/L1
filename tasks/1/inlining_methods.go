0062ab23c8cce0e592dee7f0fed68e18bc40b255
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-13 13:52:34 +0300

    do refactor

--- tasks/1/inlining_methods.go
+++ tasks/1/inlining_methods.go
@@ -5,27 +5,7 @@ import (
 	"fmt"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
-	"reflect"
-	"strings"
 )
-
-func examiner(T reflect.Type, depth int) {
-	fmt.Println(strings.Repeat("\t", depth), "Type is", T.String(), "and kind is", T.Kind())
-
-	switch T.Kind() {
-	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
-		fmt.Println(strings.Repeat("\t", depth+1), "Contained type:")
-
-		examiner(T.Elem(), depth+1)
-
-	case reflect.Struct:
-		for i := 0; i < T.NumField(); i++ {
-			f := T.Field(i)
-
-			fmt.Println(strings.Repeat("\t", depth+1), "Field", i+1, "name is", f.Name, "type is", f.Type.Name(), "and kind is", f.Type.Kind())
-		}
-	}
-}
 
 type Human struct {
 	Name   string
@@ -100,9 +80,6 @@ func NewAction(name string, age int16, height float32, weight float32) *Action {
 
 func main() {
 	action := NewAction("John", 31, 180, 80)
-	actionType := reflect.TypeOf(action)
-
-	examiner(actionType, 0)
 	fmt.Println(action)
 
 	err := action.SetName("Van")
@@ -125,7 +102,5 @@ func main() {
 		fmt.Println(err)
 	}
 
-	actionType = reflect.TypeOf(action)
-	examiner(actionType, 0)
 	fmt.Println(action)
 }
