f65d761e64edb43759702894660ca00e0416b6e1
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-14 16:49:05 +0300

    add set.go

---
+++ tasks/12/set.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func Set(slice []string) []string {
+	var result []string
+	set := make(map[string]bool, len(slice))
+
+	for _, value := range slice {
+		set[value] = true
+	}
+
+	for key := range set {
+		result = append(result, key)
+	}
+
+	return result
+}
+
+func main() {
+	slice := []string{"cat", "cat", "dog", "cat", "tree"}
+
+	set := Set(slice)
+
+	fmt.Printf("Slice representation: %v\nSet   representation: %v\n", slice, set)
+}
