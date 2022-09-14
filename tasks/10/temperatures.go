1e73b0cc9f537b9ab42d1448ffbd484a7541948f
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-14 15:55:35 +0300

    update temperatures.go

--- tasks/10/temperatures.go
+++ tasks/10/temperatures.go
@@ -1,9 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-func main() {
-	temperatures := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+func Linear(temperatures []float32) map[int][]float32 {
 	groups := make(map[int][]float32)
 
 	for _, value := range temperatures {
@@ -11,5 +13,54 @@ func main() {
 		groups[key] = append(groups[key], value)
 	}
 
-	fmt.Println(groups)
+	return groups
+}
+
+func Parallel(temperatures []float32) map[int][]float32 {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	groups := make(map[int][]float32)
+
+	for _, value := range temperatures {
+		wg.Add(1)
+
+		go func(value float32) {
+			defer wg.Done()
+
+			if value > 0 {
+				for aboveZero := 0; aboveZero < 100; aboveZero += 10 {
+					if aboveZero <= int(value) && int(value) < aboveZero+10 {
+						mu.Lock()
+						groups[int(aboveZero)] = append(groups[int(aboveZero)], value)
+						mu.Unlock()
+
+						break
+					}
+				}
+			} else {
+				for belowZero := 0; belowZero > -100; belowZero -= 10 {
+					if belowZero >= int(value) && belowZero-10 < int(value) {
+						mu.Lock()
+						groups[int(belowZero)] = append(groups[int(belowZero)], value)
+						mu.Unlock()
+
+						break
+					}
+				}
+			}
+		}(value)
+	}
+
+	wg.Wait()
+
+	return groups
+}
+
+func main() {
+	temperatures := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	groupsLinear, groupsParallel := Linear(temperatures), Parallel(temperatures)
+
+	fmt.Printf("Linear  : %v\n", groupsLinear)
+	fmt.Printf("Parallel: %v", groupsParallel)
 }
