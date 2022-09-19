f77b729023f9baac6f1743f539e7ca634403c6ca
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-19 20:03:12 +0300

    update concurrent_structure.go

--- tasks/18/concurrent_structure.go
+++ tasks/18/concurrent_structure.go
@@ -5,21 +5,25 @@ import (
 	"sync"
 )
 
+// Counter is a structure that counters with mutex for concurrent counting
 type Counter struct {
 	sync.Mutex
 	counter int
 }
 
+// NewCounter is a Counter constructor
 func NewCounter() *Counter {
 	return &Counter{}
 }
 
+// Increment is a function that increments the counter field thread-safely
 func (c *Counter) Increment() {
 	c.Lock()
 	c.counter++
 	c.Unlock()
 }
 
+// String is a function that prints the counter field
 func (c *Counter) String() string {
 	return fmt.Sprint(c.counter)
 }
@@ -30,6 +34,7 @@ func main() {
 
 	for i := 0; i < 10000; i++ {
 		wg.Add(1)
+
 		go func() {
 			defer wg.Done()
 
