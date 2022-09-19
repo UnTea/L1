6d2a8d01aff1a195cd477cc793af5be1d7f56635
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-19 20:16:29 +0300

    update big_calculator.go

--- tasks/22/big_calculator.go
+++ tasks/22/big_calculator.go
@@ -5,18 +5,22 @@ import (
 	"math/big"
 )
 
-func Add(lhs, rhs *big.Float) *big.Float {
+// Sum is a fucntion that sums two numbers and returns the result
+func Sum(lhs, rhs *big.Float) *big.Float {
 	return new(big.Float).Add(lhs, rhs)
 }
 
+// Subtraction is a fucntion that subtractions two numbers and returns the result
 func Subtraction(lhs, rhs *big.Float) *big.Float {
 	return new(big.Float).Sub(lhs, rhs)
 }
 
+// Multiply is a fucntion that multiplies two numbers and returns the result
 func Multiply(lhs, rhs *big.Float) *big.Float {
 	return new(big.Float).Mul(lhs, rhs)
 }
 
+// Divide is a fucntion that divides two numbers and returns the result
 func Divide(lhs, rhs *big.Float) *big.Float {
 	return new(big.Float).Quo(lhs, rhs)
 }
@@ -28,7 +32,7 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 
-	fmt.Println("Add is equal: ", Add(a, b))
+	fmt.Println("Sum is equal: ", Sum(a, b))
 	fmt.Println("Subtraction is equal: ", Subtraction(a, b))
 	fmt.Println("Multiply is equal: ", Multiply(a, b))
 	fmt.Println("Division is equal: ", Divide(a, b))
