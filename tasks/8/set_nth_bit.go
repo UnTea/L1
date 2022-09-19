f2ff4094a173db17194db2d445335ae2e273090f
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-19 19:17:27 +0300

    update set_nth_bit.go

--- tasks/8/set_nth_bit.go
+++ tasks/8/set_nth_bit.go
@@ -5,8 +5,9 @@ import (
 	"strconv"
 )
 
-func ChangeNthBit(number *int64, position uint, value uint) {
-	check := *number&(1<<int64(position)) != 0 // checking for the presence of a bit
+func ChangeNthBit(number *int64, position int64, value int64) {
+	position -= 1                       // reduction to the zero-based numbering form
+	check := *number&(1<<position) != 0 // checking for the presence of a bit
 
 	if check && (value == 1) || !check && (value == 0) { // checking cases when the bit is already in the desired position
 		return
@@ -17,10 +18,26 @@ func ChangeNthBit(number *int64, position uint, value uint) {
 	}
 }
 
+func ChangeNthBitByMasks(number *int64, position int64, value int64) {
+	position -= 1                // reduction to the zero-based numbering form
+	*number &= ^(1 << position)  // zeroed a bit in position
+	*number |= value << position // bitwise or for valued a bit at position
+}
+
 func main() {
 	number := int64(43690)
 
 	fmt.Println(strconv.FormatInt(number, 2), strconv.FormatInt(number, 10)) // 1010101010101010 43690
-	ChangeNthBit(&number, 10, 1)
+	ChangeNthBit(&number, 11, 1)
 	fmt.Println(strconv.FormatInt(number, 2), strconv.FormatInt(number, 10)) // 1010111010101010 44714
+
+	ChangeNthBitByMasks(&number, 2, 0)
+	fmt.Println(strconv.FormatInt(number, 2), strconv.FormatInt(number, 10))
+
+	ChangeNthBitByMasks(&number, 4, 0)
+	fmt.Println(strconv.FormatInt(number, 2), strconv.FormatInt(number, 10))
+
+	ChangeNthBitByMasks(&number, 2, 1)
+	fmt.Println(strconv.FormatInt(number, 2), strconv.FormatInt(number, 10))
+
 }
