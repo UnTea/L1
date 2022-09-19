b906a205d11d5618384297088bddaf42a9c1aa6d
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-19 15:25:49 +0300

    add binary_search.go

---
+++ tasks/17/binary_search.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func binarySearch(array []int, target int) int {
+	min, max := 0, len(array)-1
+
+	for min <= max {
+		mid := min + ((max - min) / 2)
+
+		switch {
+		case array[mid] == target:
+			return mid
+		case array[mid] > target:
+			max = mid - 1
+		case array[mid] < target:
+			min = mid + 1
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	sortedArray := []int{-2, -1, 0, 1, 2, 3, 4, 5, 10, 20, 50, 100, 120}
+
+	fmt.Println(binarySearch(sortedArray, 3))
+	fmt.Println(sort.SearchInts(sortedArray, 3))
+}
