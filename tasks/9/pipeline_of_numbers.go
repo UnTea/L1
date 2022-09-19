ba9fce73fdb9f529b38594fe5b661ee9337224dd
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-19 19:28:27 +0300

    update pipeline_of_numbers.go

--- tasks/9/pipeline_of_numbers.go
+++ tasks/9/pipeline_of_numbers.go
@@ -5,38 +5,41 @@ import (
 	"sync"
 )
 
-func Square(in <-chan int, out chan<- int) {
-	for val := range in {
-		square := val * val
-		out <- square
+// Square is a function that squares values from input channel and sen then to output channel
+func Square(input <-chan int, output chan<- int) {
+	for value := range input {
+		square := value * value
+		output <- square
 	}
 
-	close(out)
+	close(output)
 }
 
-func Read(out <-chan int, wg *sync.WaitGroup) {
+// Read is a function that reads data from input channel and prints it
+func Read(input <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for val := range out {
-		fmt.Println(val)
+	for value := range input {
+		fmt.Println(value)
 	}
 }
 
 func main() {
-	in, out := make(chan int, 10), make(chan int, 10)
-	arr := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	input, output := make(chan int, 10), make(chan int, 10)
+	array := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	go Square(in, out)
+	go Square(input, output)
 
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	go Read(out, &wg)
+	go Read(output, &wg)
 
-	for _, val := range arr {
-		in <- val
+	for _, value := range array {
+		input <- value
 	}
-	close(in)
+
+	close(input)
 
 	wg.Wait()
 }
