85886289a459ebfc00d56127bae1f76ec8c11ece
Egor Aksenov <weqqu2014@yandex.ru>
2022-09-19 18:54:15 +0300

    update concurrency_map.go

--- tasks/7/concurrency_map.go
+++ tasks/7/concurrency_map.go
@@ -5,18 +5,21 @@ import (
 	"sync"
 )
 
+// Map is a thread safe structure that permits writing values from multiple goroutines
 type Map struct {
 	sync.Mutex
 	sync.WaitGroup
 	m map[int]int
 }
 
+// NewMap is a constructor of Map structure
 func NewMap() *Map {
 	return &Map{
 		m: make(map[int]int),
 	}
 }
 
+// SetValue is a function that sets values into Map object
 func (m *Map) SetValue(id int) {
 	m.Lock()
 	for i := 0; i <= 10; i++ {
